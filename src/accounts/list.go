1b7cec2fba18dee4fe70e32c442decebd64dece5
cael <adolfoecs@gmail.com>
2020-01-31 07:45:04 +0000

    moving atttributes struts to accounts

--- src/accounts/list.go
+++ src/accounts/list.go
@@ -32,19 +32,6 @@ func (cli DefaultAccountsClient) List() (Account, error) {
 	return accounts, err
 }
 
-type Attributes struct {
-	Country                  string `json:"country"`
-	Base_currency            string `json:"base_currency"`
-	Account_number           string `json:"account_number"`
-	Bank_id                  string `json:"bank_id"`
-	Bank_id_code             string `json:"bank_id_code"`
-	Bic                      string `json:"bic"`
-	Iban                     string `json:"iban"`
-	Account_classification   string `json:"account_classification"`
-	Join_account             bool   `json:"join_account"`
-	Account_matching_opt_out bool   `json:"account_matching_opt_out"`
-}
-
 type AccountDetail struct {
 	Type            string     `json:"type"`
 	Id              string     `json:"id"`
